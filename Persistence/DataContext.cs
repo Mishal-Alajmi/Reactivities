58a6d08e0f7c61f4ec205ffb0c5ac9f4118f844a
Mishal <mishal.ajm@gmail.com>
2020-08-24 09:21:51 +0300

    Added API Handlers

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options): base(options)
-        {
-        }
+        public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder) 
+        public DbSet<Activity> Activities { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Value>()
                 .HasData(
-                new Value { Id = 1, Name = "Mishal"},
-                new Value { Id = 2, Name = "Ibrahim"},
-                new Value { Id = 3, Name = "Khalaf"},
-                new Value { Id = 4, Name = "Zack"},
-                new Value { Id = 5, Name = "Faisal"},
-                new Value { Id = 6, Name = "Abduallah"},
-                new Value { Id = 7, Name = "Bader"}
-            );
+                    new Value { Id = 1, Name = "Mishal" },
+                    new Value { Id = 2, Name = "Ibrahim" },
+                    new Value { Id = 3, Name = "Khalaf" },
+                    new Value { Id = 4, Name = "Zack" },
+                    new Value { Id = 5, Name = "Faisal" },
+                    new Value { Id = 6, Name = "Abduallah" },
+                    new Value { Id = 7, Name = "Bader" }
+                );
         }
     }
 }
