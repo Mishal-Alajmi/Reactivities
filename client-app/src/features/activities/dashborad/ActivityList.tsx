5c1ce6bf18574a556611db6ceecd2b7652eb2e99
Mishal <mishal.ajm@gmail.com>
2020-09-02 13:07:37 +0300

    Integrated database with client app

--- client-app/src/features/activities/dashborad/ActivityList.tsx
+++ client-app/src/features/activities/dashborad/ActivityList.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean;
+  target: string;
 }
 
 export const ActivityList: React.FC<IProps> = ({
   activities,
   selectActivity,
   deleteActivity,
+  submitting,
+  target,
 }) => {
   return (
-    <Segment clearing raised textAlign="left">
+    <Segment clearing raised textAlign='left'>
       <Item.Group divided>
         {activities.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as='a'>{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
@@ -29,16 +33,18 @@ export const ActivityList: React.FC<IProps> = ({
               </Item.Description>
               <Item.Extra>
                 <Button
-                  floated="right"
-                  content="View"
-                  color="blue"
+                  floated='right'
+                  content='View'
+                  color='blue'
                   onClick={() => selectActivity(activity.id)}
                 />
                 <Button
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                  onClick={() => deleteActivity(activity.id)}
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                  onClick={(e) => deleteActivity(e, activity.id)}
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
   setEditMode: (editMode: boolean) => void;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
@@ -15,6 +16,7 @@ export const ActivityForm: React.FC<IProps> = ({
   activity: initialFormActivity,
   createActivity,
   editActivity,
+  submitting,
 }) => {
   const initializeForm = () => {
     if (initialFormActivity) {
@@ -55,48 +57,54 @@ export const ActivityForm: React.FC<IProps> = ({
     <Segment clearing>
       <Form onSubmit={handleSumbit}>
         <Form.Input
-          placeholder="Title"
+          placeholder='Title'
           value={activity.title}
-          name="title"
+          name='title'
           onChange={handleInputChange}
         />
         <Form.TextArea
           rows={2}
-          placeholder="Description"
+          placeholder='Description'
           value={activity.description}
-          name="description"
+          name='description'
           onChange={handleInputChange}
         />
         <Form.Input
-          placeholder="Category"
+          placeholder='Category'
           value={activity.category}
-          name="category"
+          name='category'
           onChange={handleInputChange}
         />
         <Form.Input
-          type="date"
-          placeholder="Date"
+          onChange={handleInputChange}
+          name='date'
+          type='datetime-local'
+          placeholder='Date'
           value={activity.date}
-          name="datetime-local"
-          onChange={handleInputChange}
         />
         <Form.Input
-          placeholder="City"
+          placeholder='City'
           value={activity.city}
-          name="city"
+          name='city'
           onChange={handleInputChange}
         />
         <Form.Input
-          placeholder="Venue"
+          placeholder='Venue'
           value={activity.venue}
-          name="venue"
+          name='venue'
           onChange={handleInputChange}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
         <Button
-          floated="right"
-          type="button"
-          content="Cancel"
+          loading={submitting}
+          floated='right'
+          positive
+          type='submit'
+          content='Submit'
+        />
+        <Button
+          floated='right'
+          type='button'
+          content='Cancel'
           onClick={() => setEditMode(false)}
         />
       </Form>
